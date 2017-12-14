60ea0631110a127092b90005a194e544df1b13cb
Maurice Butler <maurice.butler@gmail.com>
2017-12-14 13:32:58 +1000

    only fire event if element still exists

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-function fireEvent(element, name){
+function fireEvent(element, name) {
+    if (!element) {
+        return;
+    }
+
     var event;
 
     if (document.createEventObject) {
         event = document.createEventObject();
         return element.fireEvent('on' + name, event);
     }
-    else {
-        event = document.createEvent('HTMLEvents');
-        event.initEvent(name, true, true );
-        return !element.dispatchEvent(event);
-    }
+
+    event = document.createEvent('HTMLEvents');
+    event.initEvent(name, true, true);
+    return !element.dispatchEvent(event);
 }
 
 module.exports = fireEvent;
